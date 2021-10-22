4a1e65e006ce403499941111e0fb0f5e4e0df4c6
Sergio <ikbav@plaiaundi.net>
2021-10-22 13:46:19 +0200

    Agregar archivos de proyecto.

---
+++ TPV/FormLogin.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace TPV
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbUsuario = new System.Windows.Forms.TextBox();
+            this.tbContrasena = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(188, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pantalla de Login";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(223, 323);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(176, 36);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Iniciar sesión";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(115, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(408, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contraseña";
+            // 
+            // tbUsuario
+            // 
+            this.tbUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUsuario.Location = new System.Drawing.Point(78, 218);
+            this.tbUsuario.Name = "tbUsuario";
+            this.tbUsuario.Size = new System.Drawing.Size(177, 26);
+            this.tbUsuario.TabIndex = 3;
+            // 
+            // tbContrasena
+            // 
+            this.tbContrasena.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbContrasena.Location = new System.Drawing.Point(395, 218);
+            this.tbContrasena.Name = "tbContrasena";
+            this.tbContrasena.Size = new System.Drawing.Size(177, 26);
+            this.tbContrasena.TabIndex = 4;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 394);
+            this.Controls.Add(this.tbContrasena);
+            this.Controls.Add(this.tbUsuario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label1);
+            this.Name = "FormLogin";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbUsuario;
+        private System.Windows.Forms.TextBox tbContrasena;
+    }
+}
+
---
+++ TPV/FormLogin.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace TPV
+{
+    public partial class FormLogin : Form
+    {
+       
+        public FormLogin()
+        {
+            InitializeComponent();
+
+
+            
+        }
+
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+            OleDbConnection conexion = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:/Users/2DAM3/source/repos/TPV/TPV/Database1.accdb");
+
+            conexion.Open();
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT * FROM Usuarios", conexion);
+
+            DataSet d = new DataSet();
+            adapter.Fill(d);
+
+            Boolean correcto = false;
+
+            foreach (DataRow row in d.Tables[0].Rows)
+            {
+                Console.WriteLine("ID: " + row["Id"]);
+                Console.WriteLine("Usuario: " + row["Usuario"]);
+                Console.WriteLine("Contraseña: " + row["Contrasena"]);
+                Console.WriteLine("Es Admin: " + row["Admin"]);
+                Console.WriteLine("");
+
+
+                if (tbUsuario.Text.Equals(row["Usuario"]) && tbContrasena.Text.Equals(row["Contrasena"]))
+                {
+                    correcto = true;
+
+                    if ((Boolean)row["Admin"] == true){
+
+                        FormAdmin admin = new FormAdmin();
+                        admin.Show();
+                        this.Hide();
+
+                    } else
+                    {
+                        FormUsuario usuario = new FormUsuario();
+                        usuario.Show();
+                        this.Hide();
+                    }
+                }
+            }
+
+            if(!correcto){
+                MessageBox.Show("Usuario incorrecto, introduce de nuevo los datos!!");
+            }
+
+            conexion.Close();
+
+            Console.ReadLine();
+
+
+
+            
+        }
+    }
+}
